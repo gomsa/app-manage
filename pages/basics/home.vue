bcc486283b4b98a5a2252ab06510d6bbaadc813c
bigrocs <bigrocs@qq.com>
2019-06-23 18:04:32 +0800

    自动提交 git 代码

---
+++ pages/basics/home.vue
@@ -0,0 +1,85 @@
+<template name="basics">
+	<view>
+		<scroll-view scroll-y class="page">
+			<image src="/static/images/banner1.png"
+			 mode="widthFix" class="response"></image>
+			<view class="nav-list">
+				<navigator hover-class="none" :url="'/pages/basics/' + item.name" class="nav-li" navigateTo :class="'bg-'+item.color"
+				 :style="[{animation: 'show ' + ((index+1)*0.2+1) + 's 1'}]" v-for="(item,index) in elements" :key="index">
+					<view class="nav-title">{{item.title}}</view>
+					<view class="nav-name">{{item.name}}</view>
+					<text :class="'gomsaIcon-' + item.cuIcon"></text>
+				</navigator>
+			</view>
+			<view class="cu-tabbar-height"></view>
+		</scroll-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "basics",
+		data() {
+			return {
+				elements: [{
+						title: '收银',
+						name: 'cashier',
+						color: 'cyan',
+						cuIcon: 'cashier'
+					},
+					{
+						title: '盘点',
+						name: 'check',
+						color: 'blue',
+						cuIcon: 'pandian'
+					},
+					{
+						title: '商品 ',
+						name: 'goods',
+						color: 'mauve',
+						cuIcon: 'goods'
+					},
+					{
+						title: '库存',
+						name: 'stock',
+						color: 'pink',
+						cuIcon: 'stock'
+					},
+					{
+						title: '结算',
+						name: 'settlement',
+						color: 'purple',
+						cuIcon: 'settlement'
+					},
+					{
+						title: '供应商',
+						name: 'vendor',
+						color: 'brown',
+						cuIcon: 'vendor'
+					},
+					{
+						title: '用户',
+						name: 'users',
+						color: 'red',
+						cuIcon: 'users'
+					},
+					{
+						title: '系统',
+						name: 'system',
+						color: 'orange',
+						cuIcon: 'system'
+					}
+				],
+			};
+		},
+		onShow() {
+			console.log("success")
+		}
+	}
+</script>
+
+<style>
+	.page {
+		height: 100vh;
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,51 +1,54 @@
 <template>
 	<view>
-		<view class="content">
-			<image class="logo" src="/static/logo.png"></image>
-			<view>
-				<text class="title">{{title}}</text>
-			</view>
-		</view>
+		<basics v-if="PageCur=='basics'"></basics>
 		<!-- cu-bar tabbar bg-white shadow foot -->
 		<view class="cu-bar tabbar bg-white shadow foot">
-			<view class="action text-green">
-				<view class="cuIcon-homefill"></view> 首页
-			</view>
-			<view class="action text-gray">
-				<view class="cuIcon-similar"></view> 分类
-			</view>
-			<view class="action text-gray add-action">
-				<button class="cu-btn cuIcon-add bg-green shadow"></button>
-				发布
-			</view>
-			<view class="action text-gray">
-				<view class="cuIcon-cart">
-					<view class="cu-tag badge">99</view>
-				</view>
-				购物车
-			</view>
-			<view class="action text-gray">
-				<view class="cuIcon-my">
-					<view class="cu-tag badge"></view>
-				</view>
-				我的
+			<view 
+				v-for="(itme,key) in tabbar"
+				:key="key"
+				class="action" 
+				:class="[itme.click ? 'text-green' : ' text-gray', itme.action ? 'add-action':'']"
+				@click="NavChange" 
+				:data-cur="key"
+			>
+				<view :class=[itme.icon]>
+					<view v-if="itme.info > 0" class="cu-tag badge">{{itme.info}}</view>
+				</view> {{itme.title}}
 			</view>
 		</view>
 	</view>
 </template>
 
 <script>
+	import basics from '@/pages/basics/home.vue'
 	export default {
+		components: { basics },
 		data() {
 			return {
-				title: 'Hello1'
+				PageCur: 'basics',
+				tabbar:{
+					basics: {title: '首页', icon:'cuIcon-homefill',info:0, click: true, action: false},
+					info: {title: '消息', icon:'cuIcon-comment',info:49, click: false, action: false},
+					cash: {title: '收银', icon:'cu-btn cuIcon-scan bg-green shadow',info:0, click: false, action: true},
+					report: {title: '报表', icon:'cuIcon-rankfill',info:0, click: false, action: false},
+					my: {title: '我的', icon:'cuIcon-my',info:0, click: false, action: false},
+				}
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+			NavChange: function(e) {
+				this.PageCur = e.currentTarget.dataset.cur
+				this.TabbarClick()
+			},
+			TabbarClick(){
+				for (const key in this.tabbar) {
+					this.tabbar[key].click = false
+				}
+				this.tabbar[this.PageCur].click = true
+			}
 		}
 	}
 </script>
